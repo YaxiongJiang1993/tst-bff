444bb4ae4e77b4add7cc3e821c79558ea4765222
davih <2014405707@qq.com>
2023-05-21 22:47:21 +0800

    tmp

--- src/main/java/com/tst/bff/App.java
+++ src/main/java/com/tst/bff/App.java
@@ -2,10 +2,12 @@ package com.tst.bff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author Yaxio
  */
+@EnableFeignClients
 @SpringBootApplication
 public class App {
 
